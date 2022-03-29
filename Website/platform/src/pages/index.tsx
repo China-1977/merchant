c1ab8609ec9d42b2ae00d85fa5811a7d165f0555
wangchangkang <for_ccc@qq.com>
2022-03-29 15:28:20 +0800

    .

--- Website/platform/src/pages/index.tsx
+++ Website/platform/src/pages/index.tsx
@@ -1,9 +1,45 @@
-import styles from './index.less';
+import React from 'react';
+import mapboxgl from 'mapbox-gl';
 
+
+/**
+ * version：样式版本，当前必须必须设置8。
+ name：样式名称，设置一个可读的名称描述即可。
+ sprite：mapbox地图使用的图标。
+ glyphs：mapbox地图使用的标注字体。
+ sources： mapbox地图使用的地图服务资源定义。
+ layers： mapbox地图使用的图层定义。
+ *
+ * @constructor
+ */
 export default function IndexPage() {
+  React.useEffect(() => {
+    mapboxgl.accessToken = 'pk.eyJ1IjoiYW56aGlodW4iLCJhIjoiY2lsdnhjdjN5MDFvMHVia3NpYTlnbmUzaSJ9.twlExCjpR7uwH2IiFC7aDA';
+    var map = new mapboxgl.Map({
+      container: 'map',
+      style:{
+        'version': 8,
+        'sources': {
+          'raster-tiles': {
+            'type': 'raster',
+            'tiles': [
+              'http://127.0.0.1:8010/{z}/{x}/{y}.png'
+            ],
+          }
+        },
+        'layers': [{
+          'id': 'simple-tiles',
+          'type': 'raster',
+          'source': 'raster-tiles',
+          'minzoom': 0,
+          'maxzoom': 22
+        }]
+      },
+      center: [116.85059 ,36.78289],
+      zoom: 3
+    });
+  });
   return (
-    <div>
-      <h1 className={styles.title}>Page index</h1>
-    </div>
+    <div id="map" style={{width: "100%", height: "100vh"}} className="marker"/>
   );
 }
