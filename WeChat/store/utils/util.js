6a3d1a92b8198a4799b4fdb0e0134add97e3daa2
wangchangkang <15063517240@139.com>
2022-04-10 22:55:43 +0800

    .

--- WeChat/store/utils/util.js
+++ WeChat/store/utils/util.js
@@ -2,7 +2,7 @@ const app = getApp();
 const { windowWidth } = app.globalData;
 const size = 15;
 const domain = 'https://1977.work';
-// const domain = 'http://127.0.0.1:5010';
+// const domain = 'http://192.168.0.4:5010';
 const appid = "wx950ae546eec14733";
 const scoreStatus = {
   WAIT_PAY: "待支付",
