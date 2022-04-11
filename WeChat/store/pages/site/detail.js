6003306fd74a9c8886577a5de2e4060fbb91b2ca
wangchangkang <for_ccc@qq.com>
2022-04-11 10:42:09 +0800

    .

--- WeChat/store/pages/site/detail.js
+++ WeChat/store/pages/site/detail.js
@@ -1,66 +1,130 @@
-// pages/site/detail.js
+import { domain, checkStore, wxRequest } from '../../utils/util.js';
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    location: {},
+    code: [],
+    value: []
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
+    this.setData({
+      code: [],
+      value: []
+    })
+    if (options.index) {
+      let pages = getCurrentPages();//当前页面栈
+      let prevPage = pages[pages.length - 2];//上一页面
+      const site = prevPage.data.sites[options.index];
+      this.setData({
+        ...site, index: options.index
+      })
+    }
+  },
+
+  chooseLocation: function (e) {
+    wx.authorize({
+      scope: 'scope.userLocation',
+      success: (res) => {
+        const { location, detail } = this.data;
+        if (location && location.x && location.y) {
+          wx.chooseLocation({
+            longitude: location.x,
+            latitude: location.y,
+            name: detail,
+            success: (res) => {
+              this.setData({ location: { x: res.longitude, y: res.latitude }, detail: res.site, name: res.name });
+            }
+          })
+        } else {
+          wx.getLocation({
+            type: 'gcj02',
+            success: (res) => {
+              wx.chooseLocation({
+                longitude: parseFloat(res.longitude),
+                latitude: parseFloat(res.latitude),
+                success: (res) => {
+                  this.setData({ location: { x: res.longitude, y: res.latitude }, detail: res.site, name: res.name });
+                }
+              })
+            }
+          });
+        }
+      },
+      fail: (res) => {
+        wx.showModal({
+          title: '提示',
+          content: '请允许小程序使用位置消息',
+          confirmColor: '#e64340',
+          showCancel: false,
+          success: () => {
+            wx.openSetting();
+          }
+        })
+      }
+    })
+
 
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  saveSite: function (e) {
+    checkStore().then(({ authorization, info }) => {
+      const { value, code, postcode, location, index } = this.data;
+      let site = { ...e.detail.value, value, code, postcode, location };
+      wxRequest({
+        url: `${domain}/store/sites`,
+        header: { authorization, ...info },
+        data: site,
+        method: "POST",
+      }).then((data) => {
+        let pages = getCurrentPages();//当前页面栈
+        let prevPage = pages[pages.length - 2];//上一页面
+        if (index) {
+          const key = `sites[${index}]`;
+          prevPage.setData({
+            [key]: site
+          });
+        } else {
+          let sites = prevPage.data.sites;
+          sites.unshift(site);
+          prevPage.setData({
+            sites
+          });
+        }
+        this.setData({
+          ...data
+        });
+        wx.navigateBack({
+          delta: 1
+        });
+      });
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
+  deleteSite: function (e) {
 
+    wx.showModal({
+      title: '警示',
+      content: '确定删除吗？',
+      success: (res) => {
+        if (res.confirm) {
+          checkStore().then(({ authorization, info }) => {
+            const { id } = this.data;
+            wxRequest({
+              url: `${domain}/store/sites/${id}`,
+              method: "DELETE",
+              header: { authorization, ...info },
+            }).then((data) => {
+              wx.navigateBack({
+                delta: 2
+              });
+            });
+          })
+        }
+      }
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  bindRegionChange: function (e) {
+    this.setData({ ['code']: e.detail.code, ['postcode']: e.detail.postcode, ['value']: e.detail.value });
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
