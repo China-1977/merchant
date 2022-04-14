0a9d9af06c3f6ec5837c8ac088499948e3420bce
wangchangkang <15063517240@139.com>
2022-04-14 22:36:49 +0800

    .

--- App/HeroMan/app/src/main/java/work/onss/heroman/ui/screens/MainFrame.kt
+++ App/HeroMan/app/src/main/java/work/onss/heroman/ui/screens/MainFrame.kt
@@ -1,14 +1,15 @@
 package work.onss.heroman.ui.screens
 
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import work.onss.heroman.model.entity.NavigationItem
@@ -27,37 +28,46 @@ fun MainFrame() {
         mutableStateOf(0)
     }
 
-    Scaffold(bottomBar = {
-        BottomNavigation(
-            backgroundColor = MaterialTheme.colors.surface,
-        ) {
-            navigationItems.forEachIndexed { index, navigationItem ->
-
-                BottomNavigationItem(
-                    selected = currentNavigationIndex == index,
-                    onClick = {
-                        currentNavigationIndex = index
-                    },
-                    icon = {
-                        Icon(
-                            imageVector = navigationItem.icon,
-                            contentDescription = null
-                        )
-                    },
-                    label = {
-                        Text(text = navigationItem.title)
-                    },
-                )
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = "一九七七",
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .wrapContentSize(Alignment.Center)
+                    )
+                },
+                modifier = Modifier.fillMaxWidth()
+            )
+        },
+        bottomBar = {
+            BottomNavigation(backgroundColor = MaterialTheme.colors.surface) {
+                navigationItems.forEachIndexed { index, navigationItem ->
+                    BottomNavigationItem(
+                        selected = currentNavigationIndex == index,
+                        onClick = {
+                            currentNavigationIndex = index
+                        },
+                        icon = {
+                            Icon(
+                                imageVector = navigationItem.icon,
+                                contentDescription = null
+                            )
+                        },
+                        label = {
+                            Text(text = navigationItem.title)
+                        },
+                    )
+                }
             }
         }
-    }
     ) {
-        Box(modifier = Modifier.padding(it)) {
-            when (currentNavigationIndex) {
-                0 -> ScoreScreen()
-                1 -> SiteScreen()
-                2 -> MineScreen()
-            }
+        when (currentNavigationIndex) {
+            0 -> ScoreScreen()
+            1 -> SiteScreen()
+            2 -> MineScreen()
         }
     }
 }
--- App/HeroMan/app/src/main/java/work/onss/heroman/ui/screens/ScoreScreen.kt
+++ App/HeroMan/app/src/main/java/work/onss/heroman/ui/screens/ScoreScreen.kt
@@ -4,7 +4,6 @@ package work.onss.heroman.ui.screens
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -13,16 +12,11 @@ import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun ScoreScreen() {
-    TopAppBar(
-        title = {
-            Text(
-                text = "一九七七",
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .wrapContentSize(Alignment.Center)
-            )
-        },
-        modifier = Modifier.fillMaxWidth()
+    Text(
+        text = "订单",
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentSize(Alignment.Center)
     )
 }
 
