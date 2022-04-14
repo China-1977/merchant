a4fd31e75303e7db7ed1dce173a39ef7d0c77f5a
wangchangkang <15063517240@139.com>
2022-04-15 00:11:23 +0800

    .

--- App/HeroMan/app/src/main/java/work/onss/heroman/ui/screens/SiteScreen.kt
+++ App/HeroMan/app/src/main/java/work/onss/heroman/ui/screens/SiteScreen.kt
@@ -1,23 +1,36 @@
 package work.onss.heroman.ui.screens
 
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import work.onss.heroman.R
 
 
 @Composable
 fun SiteScreen() {
-    Text(
-        text = "驿站",
-        modifier = Modifier
-            .fillMaxWidth()
-            .wrapContentSize(Alignment.Center)
-    )
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        Image(
+            painter = painterResource(id = R.drawable.favicon),
+            contentDescription = null
+        )
+        Column {
+            Text(
+                text = "驿站",
+            )
+        }
+    }
+
 }
 
 @Preview
