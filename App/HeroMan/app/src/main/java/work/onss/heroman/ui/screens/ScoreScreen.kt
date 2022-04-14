72f8d10214da0640f09e61679a7448c20eff092e
wangchangkang <15063517240@139.com>
2022-04-15 00:11:23 +0800

    .

--- App/HeroMan/app/src/main/java/work/onss/heroman/ui/screens/ScoreScreen.kt
+++ App/HeroMan/app/src/main/java/work/onss/heroman/ui/screens/ScoreScreen.kt
@@ -1,23 +1,33 @@
 package work.onss.heroman.ui.screens
 
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import work.onss.heroman.R
 
 
 @Composable
 fun ScoreScreen() {
-    Text(
-        text = "订单",
-        modifier = Modifier
-            .fillMaxWidth()
-            .wrapContentSize(Alignment.Center)
-    )
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        Image(
+            painter = painterResource(id = R.drawable.favicon),
+            contentDescription = null
+        )
+        Column {
+            Text(
+                text = "驿站",
+            )
+        }
+    }
 }
 
 @Preview
