2bd9bac2179f982023e269c579dd28d8038db632
wangchangkang <for_ccc@qq.com>
2022-04-21 14:53:56 +0800

    .

--- App/HeroMan/app/src/main/java/work/onss/heroman/ui/screens/MainFrame.kt
+++ App/HeroMan/app/src/main/java/work/onss/heroman/ui/screens/MainFrame.kt
@@ -8,26 +8,34 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Person
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
-import work.onss.heroman.model.entity.NavigationItem
+import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 
+
+sealed class Screen(val route: String, val icon: ImageVector) {
+    object Score : Screen("订单", Icons.Filled.Home)
+    object Site : Screen("驿站", Icons.Filled.DateRange)
+    object Mine : Screen("我的", Icons.Filled.Person)
+}
 
 @Composable
 fun MainFrame() {
-
-    val navigationItems = listOf(
-        NavigationItem(title = "订单", icon = Icons.Filled.Home),
-        NavigationItem(title = "驿站", icon = Icons.Filled.DateRange),
-        NavigationItem(title = "我的", icon = Icons.Filled.Person),
+    val navController = rememberNavController()
+    val screens = listOf(
+        Screen.Score,
+        Screen.Site,
+        Screen.Mine
     )
-
-    var currentNavigationIndex by remember {
-        mutableStateOf(0)
-    }
-
     Scaffold(
         topBar = {
             TopAppBar(
@@ -44,32 +52,42 @@ fun MainFrame() {
         },
         bottomBar = {
             BottomNavigation(backgroundColor = MaterialTheme.colors.surface) {
-                navigationItems.forEachIndexed { index, navigationItem ->
+                val navBackStackEntry by navController.currentBackStackEntryAsState()
+                val currentDestination = navBackStackEntry?.destination
+                screens.forEachIndexed { index, screen ->
                     BottomNavigationItem(
-                        selected = currentNavigationIndex == index,
+                        selected = currentDestination?.hierarchy?.any { it.route == screen.route } == true,
                         onClick = {
-                            currentNavigationIndex = index
+                            navController.navigate(screen.route) {
+                                popUpTo(navController.graph.findStartDestination().id) {
+                                    saveState = true
+                                }
+                                launchSingleTop = true
+                                restoreState = true
+                            }
                         },
+
                         icon = {
                             Icon(
-                                imageVector = navigationItem.icon,
-                                contentDescription = null
+                                imageVector = screen.icon,
+                                contentDescription = screen.route
                             )
                         },
                         label = {
-                            Text(text = navigationItem.title)
+                            Text(text = screen.route)
                         },
                     )
                 }
             }
+        },
+        content = {
+            NavHost(navController = navController, startDestination = Screen.Score.route) {
+                composable(Screen.Score.route) { ScoreScreen() }
+                composable(Screen.Site.route) { SiteScreen() }
+                composable(Screen.Mine.route) { MineScreen() }
+            }
         }
-    ) {
-        when (currentNavigationIndex) {
-            0 -> ScoreScreen()
-            1 -> SiteScreen()
-            2 -> MineScreen()
-        }
-    }
+    )
 }
 
 @Preview
