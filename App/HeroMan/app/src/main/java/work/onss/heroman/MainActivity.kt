2f648aae73d9e11bbab7e40cb8fbaa65da1ada9d
wangchangkang <for_ccc@qq.com>
2022-04-21 14:55:58 +0800

    .

--- App/HeroMan/app/src/main/java/work/onss/heroman/MainActivity.kt
+++ App/HeroMan/app/src/main/java/work/onss/heroman/MainActivity.kt
@@ -3,11 +3,8 @@ package work.onss.heroman
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import work.onss.heroman.ui.screens.MainFrame
 import work.onss.heroman.ui.theme.HeroManTheme
