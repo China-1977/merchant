e55230802cd6bec18bad2454a467b3c8d1fdf0d0
wangchangkang <for_ccc@qq.com>
2022-04-23 16:39:14 +0800

    .

--- App/HeroMan/app/src/main/java/work/onss/heroman/screens/common/ListScore.kt
+++ App/HeroMan/app/src/main/java/work/onss/heroman/screens/common/ListScore.kt
@@ -9,10 +9,13 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.paging.LoadState
 import androidx.paging.compose.LazyPagingItems
 import androidx.paging.compose.items
+import coil.annotation.ExperimentalCoilApi
 import work.onss.heroman.data.repository.score.Score
 
 @Composable
@@ -41,9 +44,43 @@ fun ScoreList(items: LazyPagingItems<Score>) {
     LazyColumn(
         verticalArrangement = Arrangement.spacedBy(1.dp),
     ) {
+
+        if (items.loadState.refresh == LoadState.Loading) {
+            item {
+                Column(
+                    Modifier
+                        .fillMaxWidth()
+                        .padding(50.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
+
+                ) {
+                    Text(
+                        text = "加载中。。。",
+                    )
+                }
+            }
+        }
+
         items(items = items, key = { item -> item.id }) { item ->
             item?.let { ScoreItem(item = it) }
         }
+
+        if (items.loadState.append == LoadState.Loading) {
+            item {
+                Card {
+                    Column(
+                        Modifier
+                            .fillMaxWidth()
+                            .padding(50.dp),
+                    ) {
+                        Text(
+                            text = "加载中。。。",
+                        )
+                    }
+                }
+            }
+        }
+
     }
 }
 
